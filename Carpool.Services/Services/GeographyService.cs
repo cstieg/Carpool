36733213b14887637342202db59524f93458837d
cstieg <cstieg4899@yahoo.com>
2018-03-01 16:22:30 -0500

    Add: GeographyService stub

---
+++ Carpool.Services/Services/GeographyService.cs
@@ -0,0 +1,31 @@
+﻿using Carpool.Domain.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Carpool.Services.Services
+{
+    public class GeographyService
+    {
+
+        public float GetGeographicDistanceBetweenLocations(Location location1, Location location2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<float> GetRoadDistanceBetweenLocationsAsync(Location location1, Location location2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TimeSpan GetApproximateTimeBetweenLocations(Location location1, Location location2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TimeSpan> GetExactTimeBetweenLocationsAsync(Location location1, Location location2)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
